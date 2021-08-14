3ff45ded6708359c2647ab5a65f2970badf48ff3
swarajdhore <dhoreswaraj@gmail.com>
2021-08-14 22:13:50 +0530

    Latest commit

--- index.js
+++ index.js
@@ -1,10 +1,61 @@
 const express = require("express");
 
-//initialisation
-const ourAPP = express();
+// database
+const Database = require("./database");
 
-ourAPP.get("/", (request, response)=> {
-    response.json({message:"Request Served !!"});
+// initialization
+const OurAPP = express();
+
+OurAPP.get("/", (request, response) => {
+  response.json({ message: "Server is working fine!" });
 });
 
-ourAPP.listen(4000,()=> console.log("Server is running"));
+// Route    - /book
+// Des      - To get all books
+// Access   - Public
+// Method   - GET
+// Params   - none
+// Body     - none
+OurAPP.get("/book", (req, res) => {
+  return res.json({ books: Database.Book });
+});
+
+// Route    - /book/:bookID
+// Des      - To get a book based on ISBN
+// Access   - Public
+// Method   - GET
+// Params   - bookID
+// Body     - none
+OurAPP.get("/book/:bookID", (req, res) => {
+  const getBook = Database.Book.filter(
+    (book) => book.ISBN === req.params.bookID
+  );
+
+  return res.json({ book: getBook });
+});
+
+// Route    - /book/c/:category
+// Des      - to get a list of books based on category
+// Access   - Public
+// Method   - GET
+// Params   - category
+// Body     - none
+OurAPP.get("/book/c/:category", (req, res) => {
+  const getBook = Database.Book.filter((book) =>
+    book.category.includes(req.params.category)
+  );
+
+  return res.json({ book: getBook });
+});
+
+// Route    - /author
+// Des      - to get all authors
+// Access   - Public
+// Method   - GET
+// Params   - none
+// Body     - none
+OurAPP.get("/author", (req, res) => {
+  return res.json({ author: Database.Author });
+});
+
+OurAPP.listen(4000, () => console.log("Server is running"));
